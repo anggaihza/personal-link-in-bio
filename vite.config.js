d9f96e82281b9f299f3f82d036e5e747ad8ba4dd
Asep Angga Ihza S <126168431+anggaihza@users.noreply.github.com>
2025-02-23 10:04:42 +0700

    push

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/angga-arsy",
+  base: "/PERSONAL-LINK-IN-BIO",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
