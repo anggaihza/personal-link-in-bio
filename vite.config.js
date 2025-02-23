78068af7d41df871f166e75c651544b6b8d73acc
Asep Angga Ihza S <126168431+anggaihza@users.noreply.github.com>
2025-02-23 10:15:18 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/PERSONAL-LINK-IN-BIO",
+  base: "/personal-link-in-bio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
