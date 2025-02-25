28e45f1d587a725a0d82b5a4bb10c98e7d833d97
Asep Angga Ihza S <angga.asep98@gmail.com>
2025-02-25 15:14:10 +0700

    add brix template

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import LinkInBioUI from "./pages/LinkInBioUi";
+import BrixTempleteUi from "./pages/BrixTemplateUi";
 
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<LinkInBioUI />} />
+        <Route path="/brix" element={<BrixTempleteUi />} />
       </Routes>
     </Suspense>
   );
