5ab457aed7e4817c56091bfd00d7060cb717c911
Asep Angga Ihza S <angga.asep98@gmail.com>
2025-03-14 15:30:45 +0700

    add about section

--- src/components/personal/ArsyVision.jsx
+++ src/components/personal/ArsyVision.jsx
@@ -2,14 +2,74 @@ import React from "react";
 
 const ArsyVision = () => {
   return (
-    <div className="max-w-2xl mx-auto p-6 ">
+    <div className="max-w-2xl mx-auto p-6 mt-1">
       <div className="text-left mb-6">
         <h3
           className="text-2xl font-bold text-gray-800"
           style={{ fontFamily: "Cinzel Decorative, serif" }}
         >
-          Angga Arsy Sya
+          A. Angga Ihza Sya
         </h3>
+        <p
+          className="text-gray-600 mt-4 text-justify"
+          style={{ fontFamily: "PT Serif, serif" }}
+        >
+          Setelah lulus Bachelor Degree (S1) di Bandung pada tahun 2021, Arsy
+          melanjutkan pendidikan di bidang Software Engineering selama kurang
+          lebih satu tahun di salah satu kampus Jakarta dan salah satu yang
+          terbaik di Indonesia.
+        </p>
+        <p
+          className="text-gray-600 mt-2 text-justify"
+          style={{ fontFamily: "PT Serif, serif" }}
+        >
+          Saat ini Arsy adalah seorang profesional Software Engineer muslim yang
+          saat ini bekerja di salah satu perusahaan Software House di Dago
+          Bandung dan dipercaya bekerja sama dengan berbagai tim dari berbagai
+          negara dalam proyek berskala global.
+        </p>
+        <p
+          className="text-gray-600 mt-2 text-justify"
+          style={{ fontFamily: "PT Serif, serif" }}
+        >
+          Arsy terlibat dalam pengembangan Scallable Website, cloud computing,
+          dan real-time communication untuk berbagai industri. Beberapa proyek
+          yang Arsy kerjakan melibatkan kolaborasi dengan tim lintas negara,
+          dari sana Arsy mendapat pengalaman dalam menghadapi tantangan teknis
+          dan budaya kerja yang beragam.
+        </p>
+        <p
+          className="text-gray-600 mt-2 text-justify"
+          style={{ fontFamily: "PT Serif, serif" }}
+        >
+          Selain mengembangkan teknologi, Arsy suka berbagi ilmu lewat tulisan
+          dan konten. Arsy percaya bahwa teknologi bukan cuma soal membangun
+          sistem, tapi juga bagaimana bisa belajar dan berbagi dengan orang
+          lain.
+        </p>
+        <p
+          className="text-gray-600 mt-2 text-justify"
+          style={{ fontFamily: "PT Serif, serif" }}
+        >
+          Arsy dapat berkomunikasi dalam Bahasa Indonesia dan Inggris, aktif di
+          komunitas teknologi khususnya di Bandung. Sebagai seorang Muslim, Arsy
+          selalu menjunjung nilai kejujuran sebagaimana itu yang selalu di
+          tekankan di lingkungan keluarga sejak kecil, juga kerja keras, dan
+          kebermanfaatan dalam setiap pekerjaan.
+        </p>
+        <p
+          className="text-gray-600 mt-2 text-justify"
+          style={{ fontFamily: "PT Serif, serif" }}
+        >
+          Prinsip yang selalu dipegang: sebaik-baik manusia adalah orang yang
+          paling bermanfaat bagi orang lain.
+        </p>
+        <p
+          className="text-gray-600 mt-2 text-justify"
+          style={{ fontFamily: "PT Serif, serif" }}
+        >
+          Note: Arsy bukan wibu, berusaha hidup dehat dan tidak merokok.
+        </p>
       </div>
       <div className="space-y-6">
         <div>
@@ -17,14 +77,13 @@ const ArsyVision = () => {
             className="text-lg font-semibold text-gray-700"
             style={{ fontFamily: "PT Serif, serif" }}
           >
-            1. Legibility
+            1. Masa Kecil
           </h5>
           <p
             className="text-gray-600 mt-1"
             style={{ fontFamily: "PT Serif, serif" }}
           >
-            Legibility is one of the primary reasons that serifs are strongly
-            favored in writing and journalism.
+            (SOON)
           </p>
         </div>
         <div>
@@ -32,14 +91,13 @@ const ArsyVision = () => {
             className="text-lg font-semibold text-gray-700"
             style={{ fontFamily: "PT Serif, serif" }}
           >
-            2. Readability
+            2. Bandung
           </h5>
           <p
             className="text-gray-600 mt-1"
             style={{ fontFamily: "PT Serif, serif" }}
           >
-            Readability ensures smooth comprehension, making it essential for
-            effective communication.
+            (SOON)
           </p>
         </div>
         <div>
@@ -47,14 +105,13 @@ const ArsyVision = () => {
             className="text-lg font-semibold text-gray-700"
             style={{ fontFamily: "PT Serif, serif" }}
           >
-            3. Comfort
+            3. Pandangan Hidup
           </h5>
           <p
             className="text-gray-600 mt-1"
             style={{ fontFamily: "PT Serif, serif" }}
           >
-            A comfortable reading experience enhances engagement and
-            understanding, leading to better retention of information.
+            (SOON)
           </p>
         </div>
       </div>
